c2beaebf05410cb9a2bf4789f55cce31026ffa6c
nunurul <noe.rul.khoir@gmail.com>
2024-02-08 14:42:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("#open-popup").addEventListener("click", function () {
+  document.body.classList.add("active-popup");
+});
+
+document
+  .querySelector(".popup .close-btn")
+  .addEventListener("click", function () {
+    document.body.classList.remove("active-popup");
+  });
