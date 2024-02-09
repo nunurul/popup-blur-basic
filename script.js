000661dd691dc79782b544f5cf4e6376bb5268f3
nunurul <noe.rul.khoir@gmail.com>
2024-02-09 15:22:43 +0700

    menammbahkan popup banyak

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// let container = document.querySelector(".container");
+let container = document.querySelector(".container");
 
-// document.querySelector("#open-popup").addEventListener("click", function () {
-//   container.classList.add("active-popup");
-// });
+document.querySelector("#open-popup").addEventListener("click", function () {
+  container.classList.add("active-popup");
+});
 
-// document
-//   .querySelector(".popup .close-btn")
-//   .addEventListener("click", function () {
-//     container.classList.remove("active-popup");
-//   });
+document
+  .querySelector(".popup .close-btn")
+  .addEventListener("click", function () {
+    container.classList.remove("active-popup");
+  });
+
+document.querySelector("#halo-popup").addEventListener("click", function () {
+  container.classList.add("halo-popup");
+});
+
+document
+  .querySelector(".popup-2 .close-btn")
+  .addEventListener("click", function () {
+    container.classList.remove("halo-popup");
+  });
